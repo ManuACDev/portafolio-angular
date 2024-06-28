3b92de9d2a1c27ca6055056de45c5aa6a4fbd53f
ManuACDev <malamocres@gmail.com>
2024-06-28 18:37:04 +0200

    Tecnologías HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,16 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../app/components/header/header.component';
 import { SobremiComponent } from "./components/sobremi/sobremi.component";
+import { TecnologiasComponent } from "./components/tecnologias/tecnologias.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, SobremiComponent]
+    imports: [RouterOutlet, HeaderComponent, SobremiComponent, TecnologiasComponent]
 })
-export class AppComponent {
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const header = document.querySelector('header');
-    if (window.pageYOffset > 50) {
-      header?.classList.add('scrolled');
-    } else {
-      header?.classList.remove('scrolled');
-    }
-  }
+export class AppComponent {  
+ 
 }
