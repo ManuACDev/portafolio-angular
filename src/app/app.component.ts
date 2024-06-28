30ba05a4a7f7de71985cac4dd30b1df576e9d105
ManuACDev <malamocres@gmail.com>
2024-06-28 19:40:42 +0200

    Proyectos HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { HeaderComponent } from '../app/components/header/header.component';
 import { SobremiComponent } from "./components/sobremi/sobremi.component";
 import { TecnologiasComponent } from "./components/tecnologias/tecnologias.component";
 import { ExperienciaComponent } from "./components/experiencia/experiencia.component";
+import { ProyectosComponent } from "./components/proyectos/proyectos.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, SobremiComponent, TecnologiasComponent, ExperienciaComponent]
+    imports: [RouterOutlet, HeaderComponent, SobremiComponent, TecnologiasComponent, ExperienciaComponent, ProyectosComponent]
 })
 export class AppComponent {  
  
--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Proyecto {
+  nombre: string;
+  descripcion: string;
+  enlaceCodigo: string;
+  enlaceDemo: string;
+}
 
 @Component({
   selector: 'app-proyectos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './proyectos.component.html',
   styleUrl: './proyectos.component.css'
 })
 export class ProyectosComponent {
+
+  listaProyectos: Proyecto[] = [
+    {
+      nombre: 'Newsletter Sign Up Form',
+      descripcion: 'En este proyecto construyo un formulario de registro a un newsletter, acompañado de un mensaje de agradecimiento.',
+      enlaceCodigo: 'url-al-codigo',
+      enlaceDemo: 'url-a-la-demo'
+    },
+    {
+      nombre: 'Shopping Cart',
+      descripcion: 'Shopping Cart tiene la funcionalidad básica de un carrito de compras. Añade items, eliminalos, cambia las tallas e incluso prueba algunos cupones de descuento.',
+      enlaceCodigo: 'url-al-codigo',
+      enlaceDemo: 'url-a-la-demo'
+    }
+  ];
 
 }
