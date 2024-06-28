15cc82bced7b7ba7bce0657157d1bcadc30f2456
ManuACDev <malamocres@gmail.com>
2024-06-28 19:32:10 +0200

    Experiencia HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../app/components/header/header.component';
 import { SobremiComponent } from "./components/sobremi/sobremi.component";
 import { TecnologiasComponent } from "./components/tecnologias/tecnologias.component";
+import { ExperienciaComponent } from "./components/experiencia/experiencia.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, SobremiComponent, TecnologiasComponent]
+    imports: [RouterOutlet, HeaderComponent, SobremiComponent, TecnologiasComponent, ExperienciaComponent]
 })
 export class AppComponent {  
  
--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-experiencia',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experiencia.component.html',
   styleUrl: './experiencia.component.css'
 })
 export class ExperienciaComponent {
+
+  experiencias = [
+    {
+      puesto: 'Desarrollador Front-End',
+      empresa: 'Tech Solutions',
+      fecha: 'Enero 2022 - Presente',
+      tareas: [
+        'Desarrollo y mantenimiento de interfaces de usuario',
+        'Colaboración en proyectos de software ágil',
+        'Implementación de pruebas unitarias y de integración'
+      ]
+    },
+    // Puedes añadir más experiencias laborales aquí
+  ];
 
 }
