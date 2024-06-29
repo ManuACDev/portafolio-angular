74e72a7c3a50cc08a6886b4feaaff0315baced82
ManuACDev <malamocres@gmail.com>
2024-06-29 18:39:33 +0200

    Educacion HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { SobremiComponent } from "./components/sobremi/sobremi.component";
 import { TecnologiasComponent } from "./components/tecnologias/tecnologias.component";
 import { ExperienciaComponent } from "./components/experiencia/experiencia.component";
 import { ProyectosComponent } from "./components/proyectos/proyectos.component";
+import { EducacionComponent } from "./components/educacion/educacion.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, SobremiComponent, TecnologiasComponent, ExperienciaComponent, ProyectosComponent]
+    imports: [RouterOutlet, HeaderComponent, SobremiComponent, TecnologiasComponent, ExperienciaComponent, ProyectosComponent, EducacionComponent]
 })
 export class AppComponent {  
  
