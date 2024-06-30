4fb55c7657c3c7547184b23f5995c7870cf8884f
ManuACDev <malamocres@gmail.com>
2024-06-30 22:30:37 +0200

    Enlaces Social CSS

--- src/app/components/sobremi/sobremi.component.ts
+++ src/app/components/sobremi/sobremi.component.ts
@@ -9,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class SobremiComponent {
 
+  copyToClipboard(email: string) {
+    const el = document.createElement('textarea');
+    el.value = email;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  
+    const confirmation = document.getElementById('copy-confirmation');
+    if (confirmation) {
+      confirmation.style.display = 'inline';
+      setTimeout(() => {
+        confirmation.style.display = 'none';
+      }, 2000);
+    }
+  }
+
 }
