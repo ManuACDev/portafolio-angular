0774a31b4e93ae6c705e3d0f44dad4039e25c678
ManuACDev <malamocres@gmail.com>
2024-07-19 13:43:31 +0200

    Header Responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,12 +11,43 @@ export class HeaderComponent {
 
   // Propiedad para rastrear el estado del scroll
   isScrolled = false;
+  isMenuOpen = false;
 
   // Escucha el evento de scroll
   @HostListener('window:scroll', [])
   onWindowScroll() {
     // Verifica si el scroll es mayor que 50px
-    this.isScrolled = window.pageYOffset > 50;
+    this.isScrolled = window.scrollY > 50;
+  }
+
+  ngOnInit(): void {
+    this.addSmoothScroll();
+  }
+
+  addSmoothScroll() {
+    const links = document.querySelectorAll('nav a');
+    links.forEach(link => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href')?.substring(1) || ''; // Default to empty string if null
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+          });
+        }
+        this.isMenuOpen = false; // Close the menu after clicking a link
+      });
+    });
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    const nav = document.querySelector('nav');
+    if (nav) {
+      nav.classList.toggle('show-menu', this.isMenuOpen);
+    }
   }
 
 }
