f1fb285f46f48b3208957fffcab68edb333ed46c
ManuACDev <malamocres@gmail.com>
2024-07-23 21:42:41 +0200

    Inicio Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBIyOnZLogyURv2laNyPOmWY3GjCQLVBkA",
+    authDomain: "portafolioweb-6b7c2.firebaseapp.com",
+    projectId: "portafolioweb-6b7c2",
+    storageBucket: "portafolioweb-6b7c2.appspot.com",
+    messagingSenderId: "349869658374",
+    appId: "1:349869658374:web:98a180a4507d6aae47ce87"
+  }
+};
