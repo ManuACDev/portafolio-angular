6199fc64ab79d66b2265dd8f0b8ed50ad6232b88
ManuACDev <malamocres@gmail.com>
2024-07-24 21:49:42 +0200

    Contacto Sobre Mi Firebase

--- src/app/components/sobremi/sobremi.component.ts
+++ src/app/components/sobremi/sobremi.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../../services/firestore.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sobremi',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sobremi.component.html',
   styleUrl: './sobremi.component.css'
 })
-export class SobremiComponent {
+export class SobremiComponent implements OnInit {
+
+  contacto: any;
+
+  constructor(private firestoreService: FirestoreService) { }
+
+  ngOnInit() {
+    this.getContacto();
+  }
+
+  getContacto(): void {
+    
+    const collectionPath = 'Usuario';
+    const documentId = 'Contacto';
+
+    this.firestoreService.getDocumentById(collectionPath, documentId).subscribe(
+      data => {
+        this.contacto = data;
+        console.log(this.contacto);
+      },
+      error => {
+        console.error('Error fetching document: ', error);
+      }
+    );
+  }
 
   copyToClipboard(email: string) {
     navigator.clipboard.writeText(email).then(() => {
