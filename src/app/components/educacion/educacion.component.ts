1deb6fd10f15a260217d62aac222eb152126bb3e
ManuACDev <malamocres@gmail.com>
2024-07-25 21:06:09 +0200

    Formaciones Firebase

--- src/app/components/educacion/educacion.component.ts
+++ src/app/components/educacion/educacion.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Formacion } from '../../models/formacion.model';
+import { FirestoreService } from '../../services/firestore.service';
+import { lastValueFrom } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-educacion',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './educacion.component.html',
   styleUrl: './educacion.component.css'
 })
-export class EducacionComponent {
+export class EducacionComponent implements OnInit {
+
+  formaciones: Formacion[] = [];
+
+  constructor(private firestoreService: FirestoreService) { }
+
+  ngOnInit() {
+    this.getFormacion();
+  }
+
+  async getFormacion(): Promise<void> {
+    try {
+      
+      const formaciones = await lastValueFrom(this.firestoreService.getCollection<Formacion>('Formaciones'));
+      this.formaciones = formaciones.sort((a, b) => b.id - a.id);
+
+    } catch (error) {
+      console.error('Error al obtener las formaciones: ', error);
+    }
+  }
 
 }
---
+++ src/app/models/formacion.model.ts
@@ -0,0 +1,10 @@
+export interface Formacion {
+  id: number;
+  academia: string;
+  img: string
+  grado: string;
+  ubicacion: string;
+  fechaInicio: string;
+  fechaFin: string;
+  aptitudes: string[];
+}
