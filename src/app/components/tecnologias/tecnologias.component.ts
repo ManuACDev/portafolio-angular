e9f3ad738774c7e833c318d3fbff87c7e879ca16
ManuACDev <malamocres@gmail.com>
2024-07-25 13:35:04 +0200

    Tecnologias Firebase

--- src/app/components/tecnologias/tecnologias.component.ts
+++ src/app/components/tecnologias/tecnologias.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tecnologia } from '../../models/tecnologia.model';
+import { FirestoreService } from '../../services/firestore.service';
+import { lastValueFrom } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tecnologias',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tecnologias.component.html',
   styleUrl: './tecnologias.component.css'
 })
-export class TecnologiasComponent {
+export class TecnologiasComponent implements OnInit {
+
+  tecnologias: Tecnologia[] = [];
+
+  constructor(private firestoreService: FirestoreService) { }
+
+  ngOnInit() {
+    this.getTecnologias();
+  }
+
+  async getTecnologias() {
+    try {
+      this.tecnologias = await lastValueFrom(this.firestoreService.getCollection<Tecnologia>('Tecnologias'));
+      console.log(this.tecnologias);
+    } catch (error) {
+      console.error('Error al obtener las tecnologías: ', error);
+    }
+  }
 
 }
---
+++ src/app/models/tecnologia.model.ts
@@ -0,0 +1,4 @@
+export interface Tecnologia {
+    titulo: string;
+    img: string;
+}
